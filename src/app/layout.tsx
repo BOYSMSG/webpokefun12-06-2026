66c45badd058ff34416f418c03e57a93814b6574
BOYSMSG <MSGBOYS832@GMAIL.COM>
2026-06-13 15:59:46 +0530

    Revert to static image for openGraph preview

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,10 @@ export const metadata: Metadata = {
     description: "Pokefun is a premier Cobblemon SMP server. Join our community for a unique Pokemon adventure in Minecraft!",
     images: [
       {
-        url: "https://image.thum.io/get/width/1200/crop/630/https://pokefun.in/",
+        url: "https://pokefun.in/images/home_preview.png",
         width: 1200,
         height: 630,
-        alt: "Pokefun Home Page Preview",
+        alt: "Pokefun Home Page",
       },
     ],
     siteName: "Pokefun SMP",
@@ -31,7 +31,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Pokefun - Cobblemon SMP",
     description: "Pokefun is a premier Cobblemon SMP server. Join our community for a unique Pokemon adventure in Minecraft!",
-    images: ["https://image.thum.io/get/width/1200/crop/630/https://pokefun.in/"],
+    images: ["https://pokefun.in/images/home_preview.png"],
   },
 };
 
